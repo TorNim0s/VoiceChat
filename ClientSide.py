9ddd001ff6ca0dd42091d864a83c11dab7a692b7
GunZila <liavbens@gmail.com>
2020-10-03 20:05:26 +0300

    Added Login UI

--- ClientSide.py
+++ ClientSide.py
@@ -1,22 +1,34 @@
+import tkinter as tk
 import socket
 import threading
 import pyaudio
 
 
 class Client:
-    def __init__(self):
+    def __init__(self, ip, port, canvas, login_screen):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        while 1:
-            try:
-                self.target_ip = input('Enter IP address of server --> ')
-                self.target_port = int(input('Enter target port of server --> '))
+        self.target_ip = ip
+        self.target_port = int(port)
 
-                self.s.connect((self.target_ip, self.target_port))
+        self.canvas = canvas
 
-                break
-            except:
-                print("Couldn't connect to server")
+        self.login_screen = login_screen
+
+        try:
+            self.s.connect((self.target_ip, self.target_port))
+            output = tk.Label(self.login_screen)
+            output.config(font=('David', 10), fg="firebrick2", bg="gray86", text="Connected")
+            self.canvas.create_window(200, 200, window=output)
+
+
+        except:
+            print("Couldn't connect to server")
+            output = tk.Label(self.login_screen)
+            output.config(font=('David', 10), fg="firebrick2", bg="gray86", text="Couldn't connect to server")
+            self.canvas.create_window(200, 200, window=output)
+
+            return
 
         chunk_size = 1024  # 512
         audio_format = pyaudio.paInt16
@@ -29,11 +41,12 @@ class Client:
                                           frames_per_buffer=chunk_size)
         self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
                                             frames_per_buffer=chunk_size)
-
+        self.login_screen.destroy()
         print("Connected to Server")
 
         # start threads
         receive_thread = threading.Thread(target=self.receive_server_data).start()
+
         self.send_data_to_server()
 
     def receive_server_data(self):
@@ -53,4 +66,50 @@ class Client:
                 pass
 
 
-client = Client()
+def main():
+    login_screen = tk.Tk()
+
+    canvas1 = tk.Canvas(login_screen, width=400, height=500)
+    canvas1.config(bg="gray86")
+    canvas1.pack()
+
+    Title = tk.Label(login_screen, text='Login panel')
+    Title.config(font=('helvetica 35 bold'), fg="dark turquoise", bg="gray86")
+
+    canvas1.create_window(175, 40, window=Title)
+
+    name_label = tk.Label(login_screen, text='Name:')
+    name_label.config(font=('helvetica', 10), bg="gray86")
+    canvas1.create_window(100, 100, window=name_label)
+
+    name_input = tk.Entry(login_screen)
+    canvas1.create_window(200, 100, window=name_input)
+
+    ip_label = tk.Label(login_screen, text='IP:')
+    ip_label.config(font=('helvetica', 10), bg="gray86")
+    canvas1.create_window(100, 140, window=ip_label)
+
+    ip_input = tk.Entry(login_screen)
+    canvas1.create_window(200, 140, window=ip_input)
+
+    port_label = tk.Label(login_screen, text='Port:')
+    port_label.config(font=('helvetica', 10), bg="gray86")
+    canvas1.create_window(100, 180, window=port_label)
+
+    port_input = tk.Entry(login_screen)
+    canvas1.create_window(200, 180, window=port_input)
+
+    def make_connection():
+        ip = ip_input.get()
+        port = port_input.get()
+
+        client = Client(ip, port, canvas1, login_screen)
+
+    button1 = tk.Button(text='Connect to server', command=make_connection)
+    canvas1.create_window(200, 230, window=button1)
+
+    login_screen.mainloop()
+
+
+if __name__ == "__main__":
+    main()
